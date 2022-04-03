0cd220aec8e5d2c8a75b5e3d5eae601318226ca7
simhl1123 <77651941+simhl1123@users.noreply.github.com>
2022-04-03 22:15:19 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employees.c7kn95m0dr8w.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employees"
+custombucket = "simhongliemployees"
+customregion = "us-east-1"
